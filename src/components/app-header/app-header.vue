eac88faa2c1db19a2172f6bf07caff0ae6458467
a.anpilov <a.anpilov@zebragroup.ru>
2023-04-07 00:40:01 +0300

    update base styles; removed opacity;

--- src/components/app-header/app-header.vue
+++ src/components/app-header/app-header.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="app-header">
-        <app-logo />
-        <app-navigation />
-    </header>
+  <header class="app-header">
+    <app-logo/>
+    <app-navigation/>
+  </header>
 </template>
 
 <script lang="ts">
@@ -24,25 +24,25 @@ export default defineComponent({
 
 <style lang="scss">
 .app-header {
-    display: flex;
-    align-items: center;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  pointer-events: none;
+
+  @include breakpoint(v-mobile) {
     padding: 1rem;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 100;
-    pointer-events: none;
+    background-color: $white;
+  }
 
-    @include breakpoint(v-mobile) {
-        padding: 1rem;
-        background-color: $white;
-    }
-
-    > * {
-        pointer-events: auto;
-    }
+  > * {
+    pointer-events: auto;
+  }
 }
 </style>
